849d55921ea4356be47b01b8364abd24eeccf06f
askforarun <51930990+askforarun@users.noreply.github.com>
2024-05-17 01:01:01 +0100

    Add files via upload

--- PNBFN_SI/scripts/create_polymersystems.sh
+++ PNBFN_SI/scripts/create_polymersystems.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+  
 system=(PNBF0 PNBF4 PNBF6 PNBF8 PNBF10)
 nmol=(60 32 27 23 21)
 replica=(replica_1 replica_2 replica_3)
 
+
+
+cwd=$(pwd)
+
+for i in "${system[@]}"
+do
+echo $cwd
+(cd $cwd && mkdir $i  )
+for j in "${replica[@]}"
+do
+echo $cwd/$i
+echo $j
+(cd $cwd/$i && mkdir $j  )
+done
+done
 
 
 ff_file_name=oplsaa_mod.xml
